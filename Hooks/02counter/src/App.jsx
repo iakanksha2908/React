c81abdecb30a6778fe3c37acf4ad7fbe29489215
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-06-20 16:28:02 +0000

    Hooks done

---
+++ Hooks/02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //state ko change karne k liye resp. change ko propagate kiya jata hai DOM k andar
+  let [counter, setCounter] = useState(15)
+    
+
+  function removeValue()
+  {
+      counter = counter - 1
+      if(counter < 0)
+        counter = 0
+      setCounter(counter)
+
+  }
+ 
+
+  //  let counter = 5; 
+   function addValue()
+   {
+      counter = counter + 1;
+      if(counter > 20)
+          counter = 20;
+      setCounter(counter)
+   }
+  return (
+   <>
+    <h1>React hooks</h1>
+    <h2>Counter value: {counter}</h2>
+    <button id="add" onClick={addValue}>Add value</button>
+    <button id="remove" onClick={removeValue}>Remove value</button>
+   </>
+  )
+}
+
+export default App
