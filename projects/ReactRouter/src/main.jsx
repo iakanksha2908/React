ba7545967814238ec374c9b91b2247813e761ee3
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-06-25 06:09:47 +0000

    Rwact Router Project

---
+++ projects/ReactRouter/src/main.jsx
@@ -0,0 +1,36 @@
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+
+const router = createBrowserRouter(
+
+[
+  {
+    path: '/',
+    element: <Layout />,
+    Children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <RouterProvider router={router} />
+
+  </React.StrictMode>,
+)
