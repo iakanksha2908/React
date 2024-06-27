2ea7095ff0eafcbdd439b7db85bb05ae57fa893c
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-06-27 03:54:26 +0000

    done

---
+++ projects/ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,15 @@
+import {React, useEffects} from 'react';
+import { Link, NavLink } from 'react-router-dom' 
+function Github() {
+
+    useEffects()
+
+    return (
+
+            <div>
+                <h1>React followers: </h1>
+            </div>
+    )}
+
+    export default Github; 
+    
--- projects/ReactRouter/src/components/Header/Header.jsx
+++ projects/ReactRouter/src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
                             <li>
                                 <NavLink to={"/about"}
                                     className={(isActive) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700" : "text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-black"}`
                                     }
                                 >
                                     About
@@ -56,6 +56,15 @@ function Header() {
                                     }
                                 >
                                     Contact
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={"/github"}
+                                    className={(isActive) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700" : "text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Github
                                 </NavLink>
                             </li>
                             
