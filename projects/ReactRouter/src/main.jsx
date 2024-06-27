355fcacd4958f0236127521cf55b37049c0f4635
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-06-27 01:28:25 +0000

    done

--- projects/ReactRouter/src/main.jsx
+++ projects/ReactRouter/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 
 const router = createBrowserRouter(
@@ -14,15 +15,20 @@ const router = createBrowserRouter(
   {
     path: '/',
     element: <Layout />,
-    Children: [
+    children: [
       {
         path: '',
-        element: <Home />
+        element: <Home/>
       },
       {
         path: 'about',
         element: <About />
-      }
+      },
+      {
+        path: 'contact',
+        element: <Contact/>
+      },
+
     ]
   }
 ])
