48f205401cfccc76d9929462fdb159f47a46b6aa
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-06-27 05:19:20 +0000

    done

--- projects/ReactRouter/src/components/Github/Github.jsx
+++ projects/ReactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,34 @@
-import {React, useEffects} from 'react';
-import { Link, NavLink } from 'react-router-dom' 
+import { useState } from 'react';
+import { React, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom'
 function Github() {
 
-    useEffects()
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        const myFunc = async () => {
+            try {
+                const response = await fetch("https://api.github.com/users/iakanksha2908");
+                const result = await response.json();
+                
+                setData(result);
+                console.log(result);
+            } catch (error) {
+                console.error("Error fetching the GitHub user data:", error);
+            }
+        };
+
+        myFunc();
+    }, []);
 
     return (
 
-            <div>
-                <h1>React followers: </h1>
-            </div>
-    )}
+        <div className='bg-gray-700 flexDirection-row items-denter '>
+            <div className='ml-20'>React followers: {!data ? "" : data.followers}</div>
+            <div> {!data ? "" : <img src={data.avatar_url} width={200} rounded-full></img>} </div>
 
-    export default Github; 
-    
+        </div>
+    )
+}
+
+export default Github;
