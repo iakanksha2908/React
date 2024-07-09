8247c7a72d1c3fcf9036ed9a908ed286083afde0
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-07-09 19:02:04 +0000

    todo done

--- projects/ReduxToolkitTodo/src/App.jsx
+++ projects/ReduxToolkitTodo/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import AddTodoComp from './components/AddTodo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +12,8 @@ function App() {
   return (
     <>
      <h1>Redux toolkit Project</h1>
+     <AddTodoComp/>
+     <Todos/>
     </>
   )
 }
