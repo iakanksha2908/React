9dfb6db01416740c6569cd8abb17b61be717316b
Akanksha Pawar <99631346+iakanksha2908@users.noreply.github.com>
2024-07-17 03:34:54 +0000

    auth and login done

---
+++ MegaprojectDecode.md
@@ -0,0 +1,4 @@
+<!-- react fe lib - no db, backend stuff
+api handling - how to store data, whether in the store or cookies
+Appwrite - login/signup feature -->
+Appwrite - backend as a service. Sim to firebase, but open source
---
+++ projects/MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import 
+
+
+function App() {
+ 
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <h1>App write</h1>
+    </>
+  )
+}
+
+export default App
